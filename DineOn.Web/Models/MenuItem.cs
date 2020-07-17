391865c41f873ab0ceebb3a6a02a415cf688fdc3
Nana Essandoh <nanaessandoh@gmail.com>
2020-07-17 03:08:30 -0230

    Commit after First Blazor Implementation

--- DineOn.Web/Models/MenuItem.cs
+++ DineOn.Web/Models/MenuItem.cs
@@ -11,10 +11,11 @@ namespace DineOn.Web.Models
     {
         public int MenuItemId { get; set; }
         [JsonPropertyName("img")]
+        public string Name { get; set; }
         public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public int[] Ratings { get; set; }
         public override string ToString() => JsonSerializer.Serialize<MenuItem>(this);
     }
